a17093c5e9fefb655a0574ab041668f51b33487b
jimcreel <jim.creel@gmail.com>
2023-08-30 15:39:28 -0700

    serving cached availability to minimize calls to the third party api

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const { default: axios } = require('axios');
 const app = express();
 const jwt = require('jwt-simple');
 const { OAuth2Client } = require('google-auth-library');
-const { sendNotifications } = require('./helpers/notifications.js')
+const { sendNotifications, getAvailability } = require('./helpers/notifications.js')
 
 // const sendNotifications = require('./helpers/notifications').sendNotifications;
 
@@ -44,13 +44,14 @@ app.use('/api', (req, res, next) => {
 
 
 const PORT = process.env.PORT
-
+let availabilities = {}
 app.listen(PORT, async () => {
   console.log('Server listening on port 3000');
   // call sendNotifications on a 10 minute timer
-   sendNotifications();
+  availabilities = await getAvailability()
   setInterval(async () => {
-    sendNotifications()
+    availabilities = await getAvailability()
+    sendNotifications(availabilities)
   }
   , 600000);
 });
@@ -72,6 +73,7 @@ app.use(session({
 
 // Add this middleware BELOW passport middleware
 
+
   
 
 app.use('/api/requests', require('./controllers/requests'))
@@ -87,26 +89,14 @@ app.use('/api/users', require('./controllers/users'))
 /* Mount routes/
 --------------------------------------------------------------- */
 app.get('/api/availability/:resort/:pass', async (req, res) => {
-    let resortNameUrl = req.params.resort == 'DLR' ? 'disneyland' : 'disneyworld';
-    let pass = req.params.pass
-    let headers = {
-      'authority': `${resortNameUrl}.disney.go.com`,
-      'accept': 'application/json',
-      'x-requested-with': 'XMLHttpRequest',
-      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko)',
-      'content-type': 'application/json',
-      'origin': `https://${resortNameUrl}.disney.go.com`
-    }
-    let url = `https://${resortNameUrl}.disney.go.com/passes/blockout-dates/api/get-availability/?product-types=${pass}&destinationId=DLR&numMonths=14`;
-    try {
-      const response = await axios.get(url, { headers });
-      // Process the response and send it back to the client
-      res.json(response.data);
-    } catch (error) {
-      // Handle any errors that occurred during the request
-      res.status(500).json({ error: 'An error occurred during the request.' });
-    }
-  });
+  console.log('availability route hit')
+  
+  let resort = req.params.resort;
+    
+  let pass = req.params.pass
+  let cacheAvail = availabilities[resort].filter (avail => avail.passType === pass)
+  res.json(cacheAvail)
+});
   
 
 
